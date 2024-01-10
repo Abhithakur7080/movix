d6be209f3e2f9ec8ab0e3e71bcac422c06f4ca6b
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-01-10 18:26:53 +0530

    similar movies endpoint corrected

--- src/pages/details/carousels/Similar.jsx
+++ src/pages/details/carousels/Similar.jsx
@@ -10,7 +10,7 @@ const Similar = ({ mediaType, id }) => {
       title={title}
       data={data?.results}
       loading={loading}
-      endpoint={mediaType}
+      endPoint={mediaType}
     />
   );
 };
